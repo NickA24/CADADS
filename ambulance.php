508c0e03d7f812e2b95fd7d98d1f1c73a40c6721
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-05 10:32:37 -0700

    Update ambulance.php

--- ambulance.php
+++ ambulance.php
@@ -1,7 +1,18 @@
 <?php	include_once("./inc/login.php"); include_once("./inc/header.php"); if (checklogin() != 2) { return; } ?>
-<body onload="var ele = document.getElementById('curCall'); amboInfo(ele); this.onkeydown = function(e) {console.log(e.code);};">
+<body onload="var ele = document.getElementById('curCall'); amboInfo(ele); this.onkeydown = amboShortcuts;">
 <?php if (checklogin()) { ?><div style="float:right;"><a href="inc/logout.php">Log out</a></div><?php } ?>
 <h1>Ambulance <?php echo $_SESSION['myusername'] ?>:</h1>
 <div id="curCall"></div>
+<script>
+var amboShortcuts = function(e) {
+  var str = "You have pressed a button. Press info: ";
+  foreach (var j in e)
+  {
+    str += j + ":" + e[j] + " ";
+  }
+  
+ console.log(str) 
+}
+</script>
 </body>
 </html>
