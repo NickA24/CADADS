2ec97858db8a14df7dac74278daebe1ee2667774
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-05 09:50:03 -0700

    Update ambulance.php

--- ambulance.php
+++ ambulance.php
@@ -1,11 +1,11 @@
 <?php	include_once("./inc/login.php"); include_once("./inc/header.php"); if (checklogin() != 2) { return; } ?>
 <body>
 <?php if (checklogin()) { ?><div style="float:right;"><a href="inc/logout.php">Log out</a></div><?php } ?>
-<h1>Ambulance <?php echo $_SESSION['username'] ?>:</h1>
+<h1>Ambulance <?php echo $_SESSION['myusername'] ?>:</h1>
 <div id="curCall"></div>
 <script>
     var ele = document.getElementById("curCall");
-    getJSON("./inc/getjson.php?tbl=curCall", function(err, data) {
+    getJSON("inc/getjson.php?tbl=curAmbo", function(err, data) {
     if (err !== null) {
 			ele.innerHTML = "Oops, error:" + err;
 		} else {
