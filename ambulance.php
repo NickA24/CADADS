99c019f2d51932a15b0e535c380b562c31322dbb
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-05 08:55:14 -0700

    Update ambulance.php

--- ambulance.php
+++ ambulance.php
@@ -1,6 +1,14 @@
 <?php	include_once("./inc/login.php"); include_once("./inc/header.php"); if (checklogin() != 2) { return; } ?>
 <body>
 <?php if (checklogin()) { ?><div style="float:right;"><a href="inc/logout.php">Log out</a></div><?php } ?>
-<h1>No ambulance stuff yet! come back later.</h1>
+<h1>Ambulance <?php echo $_SESSION['username'] ?>:</h1>
+<div id="curCall">
+  <?php
+  $contents = file_get_contents("./inc/getjson.php?tbl=curAmbo");
+  if ($contents !== false) {
+    echo $contents;
+  }
+  ?>
+  </div>
 </body>
 </html>
