87265e2a75597efa051f053dcd8b36ac264b2759
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-07 02:03:22 -0700

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php if (isset($title)) {echo $title;} else { echo "Welcome to the Diamond Team CAD" }</title>
+	<title><?php if (isset($title)) {echo $title;} else { echo "Welcome to the Diamond Team CAD"; }</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="/css/style.css" />
