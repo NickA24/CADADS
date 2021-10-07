3db2b72469191809edd9d7ad5456c50693f36350
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-07 01:56:30 -0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,7 +1,30 @@
 <?php include_once('./inc/login.php'); if (checklogin() != 3) { return; } ?>
 <html>
 <head><title>Admin panel</title></head>
-<body>Creating three legit users
+<body>
+	<div>Admin main functions are to Add User, Change Password, Delete user, and see ticket data.</div>
+	<br><br>
+	<div id="AddUser"><h3>Add a New User</h3></div>
+	<div id="EditPassword"><h3>Edit a User's Name or Password</h3></div>
+	<div id="DeleteUser"><h3>Delete User (Not to be used</h3></div>
+	<div id="showold"><input type="checkbox" id="inactive" name="inactive"><label for="inactive">Show Inactive</label></div>
+	<div id="ambulancetableexample"></div></center>
+	<script>
+		var x = document.getElementById("ambulancetableexample");
+		var y = document.querySelector('input[id="inactive"]');
+		var inactive = 0;
+		//This is the code to get the table to update on click.
+		y.addEventListener('click', (event) => {
+			if(y.checked) { inactive = 1; } else {inactive = 0;}
+			while (x.firstChild) {
+				x.removeChild(x.firstChild);
+			}
+			createJSTable(x, inactive);
+		});
+		//This is found in json.js, if it needs to be edited.
+		createJSTable(x, inactive);
+	</script>
+
 <?php
 //Used this code to automatically generate user files. Making the UI to create/edit/delete users and psaswords will go here.
 /*		$un = strtolower("Dispatch1");
