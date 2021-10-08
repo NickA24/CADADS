3a7dfb84ce6191c55e024b6ea5608371403410ca
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-07 17:57:16 -0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -117,7 +117,7 @@ include('./inc/header.php');
 ?>
 
 <body>
-	<?php if (checklogin()) { ?><div style="float:right;"><a href="inc/logout.php">Log out</a></div><?php } ?>
+	<?php logoutbutton(); ?>
 	<div>Admin main functions are to Add User, Change Password, Delete user, and see ticket data.</div>
 	<br><br>
 	<div id="msgBox"><?php if (isset($_SESSION['msgbox'])) {echo $_SESSION['msgbox']; unset($_SESSION['msgbox']); } ?></div>
@@ -155,49 +155,5 @@ include('./inc/header.php');
 		//This is found in json.js, if it needs to be edited.
 		ticketTable(x, inactive);
 	</script>
-
-<?php
-//Used this code to automatically generate user files. Making the UI to create/edit/delete users and psaswords will go here.
-/*		$un = strtolower("Dispatch1");
-		$pw = "dispatch";
-		$params = array(":name" => $un, ":pw" => passmake($pw), ":ut" => 1);
-		$query = "INSERT INTO users(name, hash_pw, user_type) VALUES(:name, :pw, :ut)";
-		$result = $db->query($query, $params);
-		//If this is an ambulance, create a new entry in the ambo info at that id#
-		if (1  == 2) 
-		{
-			$newid = $db->lastInsertId();
-			$params = array(":id"=>$newid);
-			$query = "INSERT INTO ambulance_info(id, status, location, lastupdate) VALUES(:id, 0, 0, NOW())";
-			$result = $db->query($query, $params);
-		}
-		$un = strtolower("Ambo1");
-		$pw = "ambulance";
-		$params = array(":name" => $un, ":pw" => passmake($pw), ":ut" => 2);
-		$query = "INSERT INTO users(name, hash_pw, user_type) VALUES(:name, :pw, :ut)";
-		$result = $db->query($query, $params);
-		//If this is an ambulance, create a new entry in the ambo info at that id#
-		if (2 == 2) 
-		{
-			$newid = $db->lastInsertId();
-			$params = array(":id"=>$newid);
-			$query = "INSERT INTO ambulance_info(id, status, location, lastupdate) VALUES(:id, 0, 0, NOW())";
-			$result = $db->query($query, $params);
-		}
-		$un = strtolower("Admin1");
-		$pw = "admin";
-		$params = array(":name" => $un, ":pw" => passmake($pw), ":ut" => 3);
-		$query = "INSERT INTO users(name, hash_pw, user_type) VALUES(:name, :pw, :ut)";
-		$result = $db->query($query, $params);
-		//If this is an ambulance, create a new entry in the ambo info at that id#
-		if (3 == 2) 
-		{
-			$newid = $db->lastInsertId();
-			$params = array(":id"=>$newid);
-			$query = "INSERT INTO ambulance_info(id, status, location, lastupdate) VALUES(:id, 0, 0, NOW())";
-			$result = $db->query($query, $params);
-		}
-*/		
-?>
 </body>
 </html>
