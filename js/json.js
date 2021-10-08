5fa5e075b3dfc9493f9317a08155c78a66aba74a
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-08 10:19:27 -0700

    Update json.js

--- js/json.js
+++ js/json.js
@@ -88,7 +88,7 @@ var createJSTable = function(ele, data, addEditData)
 		var td = document.createElement("th");
 		td.innerHTML = headdata[i];
 		tr.appendChild(td);
-		if(headdata[i] == "id" ** addEditData === 1)
+		if(headdata[i] == "id" ** addEditData == 1)
 		{
 			td.setAttribute("colspan", 3);
 		}
