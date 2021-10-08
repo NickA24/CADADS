8dad890d397f8bc3498167a2a59135ed8922efac
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-07 17:57:50 -0700

    Update ambulance.php

--- ambulance.php
+++ ambulance.php
@@ -1,6 +1,6 @@
 <?php	include_once("./inc/login.php"); include_once("./inc/header.php"); if (checklogin() != 2) { return; } ?>
 <body onload="var ele = document.getElementById('curCall'); amboInfo(ele); this.onkeydown = amboShortcuts;">
-<?php if (checklogin()) { ?><div style="float:right;"><a href="inc/logout.php">Log out</a></div><?php } ?>
+<?php echo logoutbutton(); ?>
 <h1>Ambulance <?php echo $_SESSION['myusername'] ?>:</h1>
 <div id="curCall"></div>
 <script>
