b027b623b8f2311d286d1c55a124658ec76fec62
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-10 19:51:57 -0700

    Update ambulance.php

--- ambulance.php
+++ ambulance.php
@@ -13,36 +13,35 @@
 <div id="map"></div>
 <script>
 var amboShortcuts = function(e) {
-  var str = "You have pressed a button. Press info: "+e.code+" alt:"+e.altKey+" shift:"+e.shiftKey+" ctrl:"+e.ctrlKey+" meta:"+e.metaKey+" repeat:"+e.repeat;
- console.log(str) 
+    var str = "You have pressed a button. Press info: "+e.code+" alt:"+e.altKey+" shift:"+e.shiftKey+" ctrl:"+e.ctrlKey+" meta:"+e.metaKey+" repeat:"+e.repeat;
+    console.log(str); 
 }
 
-  function initMap() {
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer();
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 34.05349, lng: -118.24532 },//over LA as default
-          zoom: 8,
-  });
+function initMap() {
+	const directionsService = new google.maps.DirectionsService();
+	const directionsRenderer = new google.maps.DirectionsRenderer();
+	const map = new google.maps.Map(document.getElementById("map"), {
+		center: { lat: 34.05349, lng: -118.24532 },//over LA as default
+		zoom: 8,
+	});
+}
 
-  directionsRenderer.setMap(map);
+directionsRenderer.setMap(map);
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  directionsService
-    .route({
-      origin: {
-        query: document.querySelector("#curCall > table > tbody > tr > td:nth-child(3)"),
-      },
-      destination: {
-        query: document.querySelector("#curCall > table > tbody > tr > td:nth-child(6)"),
-      },
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
-    .then((response) => {
-      directionsRenderer.setDirections(response);
-    })
-    .catch((e) => window.alert("Directions request failed due to " + status));
+	directionsService.route({
+		origin: {
+			query: document.querySelector("#curCall > table > tbody > tr > td:nth-child(3)"),
+		},
+		destination: {
+			query: document.querySelector("#curCall > table > tbody > tr > td:nth-child(6)"),
+		},
+		travelMode: google.maps.TravelMode.DRIVING,
+    }).then((response) => {
+		directionsRenderer.setDirections(response);
+    }).catch((e) => window.alert("Directions request failed due to " + status));
 }
+    
 </script>
   
    <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
