e6093fc78414a62e9cb8acd72d0a128c4aec82cf
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-10 20:27:58 -0700

    Update ambulance.php

--- ambulance.php
+++ ambulance.php
@@ -16,31 +16,34 @@ var amboShortcuts = function(e) {
     var str = "You have pressed a button. Press info: "+e.code+" alt:"+e.altKey+" shift:"+e.shiftKey+" ctrl:"+e.ctrlKey+" meta:"+e.metaKey+" repeat:"+e.repeat;
     console.log(str); 
 }
-
-const directionsService = new google.maps.DirectionsService();
-const directionsRenderer = new google.maps.DirectionsRenderer();
 	
 function initMap() {
+	const directionsService = new google.maps.DirectionsService();
+	const directionsRenderer = new google.maps.DirectionsRenderer();
 	const map = new google.maps.Map(document.getElementById("map"), {
 		center: { lat: 34.05349, lng: -118.24532 },//over LA as default
 		zoom: 8,
 	});
-}
 
-directionsRenderer.setMap(map);
+	directionsRenderer.setMap(map);
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-	directionsService.route({
-		origin: {
-			query: document.querySelector("#curCall > table > tbody > tr > td:nth-child(3)"),
-		},
-		destination: {
-			query: document.querySelector("#curCall > table > tbody > tr > td:nth-child(6)"),
-		},
-		travelMode: google.maps.TravelMode.DRIVING,
-    }).then((response) => {
+	function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+		directionsService.route({
+			origin: {
+				query: document.querySelector("#curCall > table > tbody > tr > td:nth-child(3)"),
+			},
+			destination: {
+				query: document.querySelector("#curCall > table > tbody > tr > td:nth-child(6)"),
+			},
+			travelMode: google.maps.TravelMode.DRIVING,
+	}).then((response) => {
 		directionsRenderer.setDirections(response);
-    }).catch((e) => window.alert("Directions request failed due to " + status));
+	}).catch((e) => window.alert("Directions request failed due to " + status));
+	
+	function testfunction()
+	{
+		calculateAndDisplayRoute(directionsService, directionsRenderer);
+	}
 }
     
 </script>
