73f83af1d128d850b0e830585722e6f9b24e58d0
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-10 19:45:58 -0700

    Update ambulance.php

--- ambulance.php
+++ ambulance.php
@@ -47,7 +47,7 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
   
    <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
     <script
-      src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&v=weekly"
+      src="https://maps.googleapis.com/maps/api/js?key=<?php include('./inc/config.php'); echo $GoogleAPIKey; ?>&callback=initMap&v=weekly"
       async
     ></script>
   
