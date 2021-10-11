a0c0953d81fd5412f14699c2a34e933c5f7bca00
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-10 20:04:27 -0700

    Update googleapi.php

--- inc/googleapi.php
+++ inc/googleapi.php
@@ -1,5 +1,4 @@
 <?php
 include('config.php');
-$ret = file_get_contents('https://maps.googleapis.com/maps/api/js?key='.$GoogleAPIKey.'&callback=initMap&v=weekly');
-var_dump($ret);
+echo file_get_contents('https://maps.googleapis.com/maps/api/js?key='.$GoogleAPIKey.'&callback=initMap&v=weekly');
 ?>
