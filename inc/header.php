98fa8857432172f8cd5351ff72cbdd5ff4a57b36
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-13 01:56:47 -0700

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -6,4 +6,5 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="/css/style.css" />
 	<script type="text/javascript" src="/js/json.js"></script>
+	<script type="text/javascript" src="/js/forms.js"></script>
 </head>
