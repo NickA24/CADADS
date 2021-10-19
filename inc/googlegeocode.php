56472071d55d59eecb10fb55b811ee87cc9a3a63
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-19 02:45:12 -0700

    Update googlegeocode.php

--- inc/googlegeocode.php
+++ inc/googlegeocode.php
@@ -3,5 +3,5 @@ if (strlen(session_id()) < 1) {session_start();}
 if (!isset($_SESSION['myusername'])) { return; }
 include('config.php');
 $address = urlencode($_GET['address']);
-echo file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.htmlentities($address).'&key='.$GoogleAPIKey);
+echo @file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.htmlentities($address).'&key='.$GoogleAPIKey);
 ?>
