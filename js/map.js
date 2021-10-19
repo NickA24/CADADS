643f379fd37aee8aff0ace940fb93bf4bb539c30
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-19 06:08:05 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -104,6 +104,8 @@ var ddMap = {
 				} else if (obj['status'] == "Available")
 				{
 					icn.url = url+"green-dot.png";	
+				} else {
+					icn.url += "red-dot.png";	
 				}
 				break;
 			case "2":
@@ -209,7 +211,7 @@ var ddMap = {
 			this.bounds.union(response.routes[0].bounds);
 			this.map.fitBounds(this.bounds);
 			//Next, do some magic with the returned data, so we have lat and long of locations. Markers REQUIRE latlong, can't use street data.
-		});//.catch((e) => console.log("Directions request failed due to " + e));
+		}).catch((e) => console.log("Directions request failed due to " + e));
 	}
 };
 
