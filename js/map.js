169a2286ba80ebd0e78347f7a6e9603daf293c66
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-19 08:14:33 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -151,8 +151,8 @@ var ddMap = {
 							obj.title = e.name;
 							if (e.source == 0) { obj.title += ": "+e.status; }
 							obj.title += ":\n"+e.location;
-							map.addMarker({lat:e.loclat, lng:e.loclng}, obj);
-							this.bounds.union({lat:e.loclat, lng:e.loclng});
+							map.addMarker({"lat":e.loclat, "lng":e.loclng}, obj);
+							this.bounds.union({"lat":e.loclat, "lng":e.loclng});
 							this.map.fitBounds(this.bounds);
 						}
 					});
