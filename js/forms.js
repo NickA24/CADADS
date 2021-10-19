8ad8ed514d4427a3ff5f6c7980c010c1114a1dfc
aluna112 <62981044+aluna112@users.noreply.github.com>
2021-10-19 15:52:32 -0700

    Update forms.js

--- js/forms.js
+++ js/forms.js
@@ -109,6 +109,7 @@ var createJSTable = function(ele, data, addEditData)
 				var btn1 = document.createElement("button");
 				btn1.setAttribute("type", "submit");
 				btn1.setAttribute("class", "edButton");
+				btn1.setAttribute("class", "edButton-primary");
 				btn1.innerHTML = "Edit";
 				form1.appendChild(hid1);
 				form1.appendChild(hid2);
@@ -135,6 +136,7 @@ var createJSTable = function(ele, data, addEditData)
 				var btn2 = document.createElement("button");
 				btn2.setAttribute("type", "submit");
 				btn2.setAttribute("class", "edButton");
+				btn2.setAttribute("class", "edButton-primary");
 				btn2.innerHTML = "Delete";
 				form2.appendChild(hid3);
 				form2.appendChild(hid4);
