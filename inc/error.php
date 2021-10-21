d813a1591602ff13161b6e4ef26f5981044ae1d0
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-21 08:13:53 -0700

    Create error.php

---
+++ inc/error.php
@@ -0,0 +1,28 @@
+<?php
+
+$status = $_SERVER['REDIRECT_STATUS'];
+$codes = array(
+		401 => array('401 Unauthorized', 'You are not authorized to view this page, or you lack valid authentication credentials'),
+		403 => array('403 Forbidden', 'The server has refused to fulfill your request.'),
+		404 => array('404 Not Found', 'The document/file requested was not found on this server.'),
+		405 => array('405 Method Not Allowed', 'The method specified in the Request-Line is not allowed for the specified resource.'),
+		408 => array('408 Request Timeout', 'Your browser failed to send a request in the time allowed by the server.'),
+		500 => array('500 Internal Server Error', 'The request was unsuccessful due to an unexpected condition encountered by the server.'),
+		502 => array('502 Bad Gateway', 'The server received an invalid response from the upstream server while trying to fulfill the request.'),
+		504 => array('504 Gateway Timeout', 'The upstream server failed to send a request in the time allowed by the server.'),
+);
+
+$title = $codes[$status][0];
+$message = $codes[$status][1];
+if ($title == false || strlen($status) != 3) {
+       $message = 'Please supply a valid status code.';
+}
+include('inc/header.php');
+// Insert headers here
+echo '<h1>'.$title.'</h1>
+<p>'.$message.'</p>';
+// Insert footer here
+?>
+<p><a href="./">Return to main page</a></p>
+</body>
+</html>
