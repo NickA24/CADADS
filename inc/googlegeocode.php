5bfccd0846a31e9dc735750e2998a7ec13a0d484
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-21 08:51:20 -0700

    Update googlegeocode.php

--- inc/googlegeocode.php
+++ inc/googlegeocode.php
@@ -1,7 +1,14 @@
 <?php
-if (strlen(session_id()) < 1) {session_start();}
-if (!isset($_SESSION['myusername'])) { return; }
-include('config.php');
-$address = urlencode($_GET['address']);
-echo @file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.htmlentities($address).'&key='.$GoogleAPIKey);
+  //This file is to return a Geocode object using the Google API. 
+  //It will take an improperly formatted address and convert it to a properly formatted human readable address, latitude, and longitude, among other features.
+  if (strlen(session_id()) < 1) {session_start();}
+  if (!isset($_SESSION['myusername'])) { return; }
+  $address = urlencode($address);
+  $url = "https://maps.googleapis.com/maps/api/geocode/json?address=".htmlentities($address);
+  include('config.php');
+  $Geocodeobj = @file_get_contents($url.'&key='.$GoogleAPIKey);
+  //If for some reason we want/need to return it as a string for javascript, rather than as a PHP object, call this file with ?returntext=1
+  if (isset($_GET['returntext'])) { echo $Geocodeobj; return; }
+  $Geocodeobj = json_decode($Geocodeobj,true);
+  return;
 ?>
