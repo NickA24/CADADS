029b333a0fc861732278661c08437195c1c079ac
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-21 08:42:43 -0700

    Update incgeo.php

--- inc/incgeo.php
+++ inc/incgeo.php
@@ -1,4 +1,6 @@
 <?php
+  //This file is to return a Geocode object using the Google API. 
+  //It will take an improperly formatted address and convert it to a properly formatted human readable address, latitude, and longitude, among other features.
   if (strlen(session_id()) < 1) {session_start();}
   if (!isset($_SESSION['myusername'])) { return; }
   $address = urlencode($address);
