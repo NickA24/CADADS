9abb2c8bcd7c179593de6348ee62a0d5da713892
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-21 08:23:01 -0700

    Create config.php

---
+++ inc/config.php
@@ -0,0 +1,10 @@
+<?php
+//This file is for storing specific API keys and other things we might not want to share publicly, but that we need on the server to function.
+
+//PDOKey is for connecting to our local MySQL server. This isn't as critical of a password because it only functions as user root on the localhost
+$PDOKey = "Your MySQL password here";
+
+//GoogleAPIKey is for connecting to any google API.
+$GoogleAPIKey = "Your API Key Here";
+return;
+?>
