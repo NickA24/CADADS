b4264d03b4530e6493dc5e32e3d7b42b6d4fa505
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-22 10:30:53 -0700

    Update closestambulance.php

--- inc/closestambulance.php
+++ inc/closestambulance.php
@@ -34,14 +34,20 @@
               <tr class="ambo1" id="ambo1">
                   <td><input type="radio" id="radioambo1" name="amboselect" value="1"></td>
                   <td>Ambo1</td>
+                  <td>Distance</td>
+                  <td>Time</td>
               </tr>
               <tr class="ambo2" id="ambo2">
                   <td><input type="radio" id="radioambo2" name="amboselect" value="2"></td>
                   <td>Ambo2</td>
+                  <td>Distance</td>
+                  <td>Time</td>
               </tr>
               <tr class="ambo3" id="ambo3">
                   <td><input type="radio" id="radioambo3" name="amboselect" value="3"></td>
                   <td>Ambo3</td>
+                  <td>Distance</td>
+                  <td>Time</td>
               </tr>
           </table>
           <button type="submit">Submit</button>
