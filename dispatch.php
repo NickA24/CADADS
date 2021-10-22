99e0bffc96a040b0dfb193e4ca04a44bd7ade9f8
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-21 20:12:58 -0700

    Update dispatch.php

--- dispatch.php
+++ dispatch.php
@@ -232,6 +232,9 @@
         text-shadow: 1px 1px 1px rgba(0,0,0,0.4);
         box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.5);
     }
+    .markerZoom {
+	cursor: pointer;    
+    }
 </style>
 <body onload="loadInit(2);">
     <nav class="sticky">
