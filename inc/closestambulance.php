7000666c86cbfabbd197adbeca60486f2368655e
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-22 09:53:29 -0700

    Update closestambulance.php

--- inc/closestambulance.php
+++ inc/closestambulance.php
@@ -22,7 +22,26 @@
         height:85%;
     }
 </style>
-  <div id="pick3"></div>
+  <div id="pick3">
+      <h3>Please choose between the closest 3 ambulances for ticket#<?php echo $_GET['id']; ?></h3>
+      <form class="chooseambulance" action="closestambulance.php" method="post">
+          <table>
+              <tr class="ambo1" id="ambo1">
+                  <td><input type="radio" id="radioambo1" name="amboselect" value="1"></td>
+                  <td>Ambo1</td>
+              </tr>
+              <tr class="ambo2" id="ambo2">
+                  <td><input type="radio" id="radioambo2" name="amboselect" value="2"></td>
+                  <td>Ambo2</td>
+              </tr>
+              <tr class="ambo3" id="ambo3">
+                  <td><input type="radio" id="radioambo3" name="amboselect" value="3"></td>
+                  <td>Ambo3</td>
+              </tr>
+          </table>
+          <button type="submit">Submit</button>
+      </form>
+  </div>
   <div id="map"></div>
 </body>
 </html>
