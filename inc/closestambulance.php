57b7491ba22c7c6d80e2f5252b299f8973171ff2
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-10-22 07:08:14 -0700

    Update closestambulance.php

--- inc/closestambulance.php
+++ inc/closestambulance.php
@@ -8,12 +8,17 @@
 
 <body onload="loadInit(3, '<?php echo $_SESSION['preferred_map']; ?>');">
     <style>
+    html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
     #map {
         position: fixed;
         bottom: 0;
         left: 0;
         right: 0;
-        top: 10%;
+        top: 15%;
         width: 100%;
         height:100%;
     }
