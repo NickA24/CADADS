de80a4b2213ff209ec1de4c9ceda813874e72707
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-11-04 19:16:09 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -292,7 +292,8 @@ var ddMap = {
 				}
 			}
 			//Next, do some magic with the returned data, so we have lat and long of locations. Markers REQUIRE latlong, can't use street data.
-		}).catch((e) => console.log("Directions request failed due to " + e));
+		}).catch((e) => {console.log("Directions request failed due to " + e); return false;});
+		return true;
 	},
 	markerprep: function(data) {
 		const ambostatus = ["Out of Service", "Available", "Enroute", "Unavailable"];
@@ -335,7 +336,8 @@ var ddMap = {
 				if (o.latlng) { map.addMarker(o.latlng, o); }
 				if (o.dlatlng) {map.addDirections(o.latlng, o.dlatlng, o.id);}
 				if (ele.initType == 3 && k > 0) {
-					map.addDirections(o.latlng, map.ticket_markers[0].position, o.id, 3, o);
+					let a = map.addDirections(o.latlng, map.ticket_markers[0].position, o.id, 3, o);
+					if (!a) { console.log("do the doo"); }
 				}
 			});
 			
