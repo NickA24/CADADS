3c0ace2fbde54e05728438a4b9a9ba2b2cc41ccc
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-11-13 22:25:48 -0800

    Add files via upload

--- inc/getjson.php
+++ inc/getjson.php
@@ -45,7 +45,7 @@
 				{
 					$where = '';
 				}
-				$sql = "SELECT ticket.id, active, ticket.name, location, lat, lng, incident_tbl.description AS incident_type, priority, IF(ticket.ambulance>0,a.name,'None') AS ambulance, IF(ticket.dispatcher>0,b.name,'None') AS dispatcher, time, comments FROM ticket LEFT JOIN incident_tbl ON ticket.incident_type = incident_tbl.id LEFT JOIN users a ON a.id=ticket.ambulance LEFT JOIN users b ON b.id=ticket.dispatcher ".$where."ORDER BY active, time";
+				$sql = "SELECT ticket.id, active, ticket.name, location, lat, lng, incident_tbl.description AS incident_type, priority, IF(priority=1, 'High/Emergency', IF(priority=2, 'Medium/Urgent', 'Low/Routine')) AS priorityText, IF(ticket.ambulance>0,a.name,'None') AS ambulance, IF(ticket.dispatcher>0,b.name,'None') AS dispatcher, time, comments FROM ticket LEFT JOIN incident_tbl ON ticket.incident_type = incident_tbl.id LEFT JOIN users a ON a.id=ticket.ambulance LEFT JOIN users b ON b.id=ticket.dispatcher ".$where."ORDER BY active, time";
 				break;
 			case 'amb':
 				//This returns all ambulance information for every ambulance, ordered by their status and the last time they updated.
