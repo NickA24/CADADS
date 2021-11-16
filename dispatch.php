94a2b7b620f005a7791ff57b80044bd24030727d
aluna112 <62981044+aluna112@users.noreply.github.com>
2021-11-16 15:32:46 -0800

    Update dispatch.php

--- dispatch.php
+++ dispatch.php
@@ -1,3 +1,7 @@
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
 <?php
     include_once("inc/login.php");
     if (checklogin() != 1) { return; }
