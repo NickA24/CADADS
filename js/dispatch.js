fc01528052ed8009ac22b8a5c35469414fbc9263
aluna112 <62981044+aluna112@users.noreply.github.com>
2021-11-23 10:41:50 -0800

    Update dispatch.js

--- js/dispatch.js
+++ js/dispatch.js
@@ -100,6 +100,17 @@ window.addEventListener('load', () => {
     const button = document.querySelector('#clear');
     button.addEventListener('click', () => {
         document.querySelector('#enter').value = "";
+	for (i = 0; i < tr.length; i++) {
+    	    td = tr[i].getElementsByTagName("td")[0];
+   	    if (td) {
+      		txtValue = td.textContent || td.innerText;
+      		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        	    tr[i].style.display = "";
+      		} else {
+        	    tr[i].style.display = "none";
+      		}
+    	    }
+  	}
     });
 });
 
