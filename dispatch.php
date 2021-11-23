be3f36df2155d4e6e5f48db4eb80133098cad3f7
aluna112 <62981044+aluna112@users.noreply.github.com>
2021-11-22 18:14:05 -0800

    Update dispatch.php

--- dispatch.php
+++ dispatch.php
@@ -10,7 +10,7 @@
     <div class="leftContainer">
 	<h1 class="sticky2 activeTicketsHeader">Active Tickets</h1>
         <button onclick="openNavAdd()" class="add btn btn-primary sticky3">Create New</button>
-	<div class="headerDiv sticky2"></div>
+	<div class="headerDiv sticky4"></div>
         <div id="mySidenav" class="sidenavAdd">
             <div id="addticket">
                 <?php
