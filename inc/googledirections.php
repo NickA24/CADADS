5c6ae17089ef567b54a8c801a56c7e57a44c59ed
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-12-02 10:34:31 -0800

    Update googledirections.php

--- inc/googledirections.php
+++ inc/googledirections.php
@@ -3,28 +3,37 @@
 	if (strlen(session_id()) < 1) {session_start();}
 	//This file is to return a direction object using the Google API. 
 	//It will take two addresses and make a path between them. Wow.
-	if (!isset($_SESSION['myusername']) || $_SESSION['user_type'] <= 0 || $_SESSION['user_type'] >= 3) { echo "no username or wrong usertype"; return; }
+	if (!isset($_SESSION['myusername']) || $_SESSION['user_type'] <= 0 || $_SESSION['user_type'] >= 3) { return; }
 	$params = array(":id"=>$_SESSION['myid']);
 	if ($_SESSION['user_type'] == 2)
 	{
-		echo "proper user type for this";
 		$sql = "SELECT loclat, loclng, dstlat, dstlng FROM ambulance_info WHERE ambulance_info.id = :id";
 		$latlng = $db->query($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
 		$latlng = $latlng[0];
 	}
-	if (isset($_GET['o'])) {$origin = urlencode($_GET['o']);} else if ($latlng['loclat'] != 0 && $latlng['loclng'] != 0) {$origin = urlencode($latlng['loclat'].",".$latlng['loclng']);}
-	if (isset($_GET['d'])) {$destination = urlencode($_GET['d']); } else if ($latlng['dstlat'] != 0 && $latlng['dstlng'] != 0) {$destination = urlencode($latlng['dstlat'].",".$latlng['dstlng']); }
+	if (isset($_GET['o'])) 
+	{
+		$origin = urlencode($_GET['o']);
+	} 
+	else
+	{
+		$origin = urlencode($latlng['loclat'].",".$latlng['loclng']);
+	}
+	if (isset($_GET['d'])) 
+	{
+		$destination = urlencode($_GET['d']); 
+	} 
+	else
+	{
+		$destination = urlencode($latlng['dstlat'].",".$latlng['dstlng']); 
+	}
 	
-	echo "now looking for destination";
 	$url = "https://maps.googleapis.com/maps/api/directions/json?origin=".$origin."&destination=".$destination;
 	include_once('config.php');
 	$directions = @file_get_contents($url.'&key='.$GoogleAPIKey);
 	//If for some reason we want/need to return it as a string for javascript, rather than as a PHP object, call this file with ?returntext=1
-	if (isset($_GET['returntext'])) { echo $directions; return; }
+	if (isset($_GET['returntext'])) { return; }
 	$directions = json_decode($directions,true);
-	var_dump($directions);
-	if (isset($_GET['dFromHome'])) { return; }
-	echo "made it here";
 	$_SESSION['location'] = $directions["routes"][0]["legs"][0]["start_address"];
 	$var['id'] = $_SESSION['myid'];
 	$var['directions'] = $directions["routes"][0]["overview_polyline"]["points"];
