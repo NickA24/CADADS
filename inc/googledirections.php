f3f8a2d31f4ec5e59c0c942d460dcca82ffab5fa
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-12-02 10:30:27 -0800

    Update googledirections.php

--- inc/googledirections.php
+++ inc/googledirections.php
@@ -12,8 +12,8 @@
 		$latlng = $db->query($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
 		$latlng = $latlng[0];
 	}
-	if (isset($_GET['o'])) {$origin = urlencode($_GET['o']);} else if ($latlng['loclat'] != 0 && $latlng['loclng'] != 0) {$origin = urlencode($latlng['loclat'].",".$latlng['loclng']);} else { return; }
-	if (isset($_GET['d'])) {$destination = urlencode($_GET['d']); } else if ($latlng['dstlat'] != 0 && $latlng['dstlng'] != 0) {$destination = urlencode($latlng['dstlat'].",".$latlng['dstlng']); } else { return; }
+	if (isset($_GET['o'])) {$origin = urlencode($_GET['o']);} else if ($latlng['loclat'] != 0 && $latlng['loclng'] != 0) {$origin = urlencode($latlng['loclat'].",".$latlng['loclng']);}
+	if (isset($_GET['d'])) {$destination = urlencode($_GET['d']); } else if ($latlng['dstlat'] != 0 && $latlng['dstlng'] != 0) {$destination = urlencode($latlng['dstlat'].",".$latlng['dstlng']); }
 	
 	echo "now looking for destination";
 	$url = "https://maps.googleapis.com/maps/api/directions/json?origin=".$origin."&destination=".$destination;
