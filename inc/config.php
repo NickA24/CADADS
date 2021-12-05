c9fd3677997946f7d812fca7d0ae7d5acdbecf75
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-12-04 22:32:17 -0800

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,12 @@
 <?php
 //This file is for storing specific API keys and other things we might not want to share publicly, but that we need on the server to function.
-
+function var_dump_ret($mixed = null) {
+  ob_start();
+  var_dump($mixed);
+  $content = ob_get_contents();
+  ob_end_clean();
+  return $content;
+}
 //PDOKey is for connecting to our local MySQL server. This isn't as critical of a password because it only functions as user root on the localhost
 $PDOKey = "Your MySQL password here";
 
