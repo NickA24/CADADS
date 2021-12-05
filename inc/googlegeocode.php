2b00de538d5b3dbc4e7e380eb99f5fb51efd6c76
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2021-12-04 23:33:19 -0800

    Update googlegeocode.php

--- inc/googlegeocode.php
+++ inc/googlegeocode.php
@@ -5,11 +5,10 @@
   if (!isset($_SESSION['myusername'])) { return; }
   $address = urlencode($address);
   $url = "https://maps.googleapis.com/maps/api/geocode/json?address=".htmlentities($address);
-  include_once('config.php');
+  include('config.php');
   $Geocodeobj = @file_get_contents($url.'&key='.$GoogleAPIKey);
   //If for some reason we want/need to return it as a string for javascript, rather than as a PHP object, call this file with ?returntext=1
   if (isset($_GET['returntext'])) { echo $Geocodeobj; return; }
-  $Geocodeobj['api'] = $GoogleAPIKey;
   $Geocodeobj = json_decode($Geocodeobj,true);
   return;
 ?>
